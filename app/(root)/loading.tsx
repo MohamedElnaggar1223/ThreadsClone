798724d33a5bea6da33af5630c79e6429b8ede68
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-01-23 11:39:33 +0200

    Loading

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,19 @@
+
+export default function Loading() {
+  return (
+    <>
+        <h1 className="head-text text-left">Home</h1>
+
+        <section className="mt-9 flex flex-col gap-10">
+            <div className=''>
+                {/* <h4 className='cursor-pointer text-base-semibold text-light-1'>{author.name}</h4> */}
+                <div className='py-24 px-3.5 mt-6 bg-gray-900/65 animate-pulse rounded-md min-w-full flex-1 my-2'/>
+                <div className='py-24 px-3.5 mt-6 bg-gray-900/65 animate-pulse rounded-md min-w-full flex-1 my-2'/>
+                <div className='py-24 px-3.5 mt-6 bg-gray-900/65 animate-pulse rounded-md min-w-full flex-1 my-2'/>
+                <div className='py-24 px-3.5 mt-6 bg-gray-900/65 animate-pulse rounded-md min-w-full flex-1 my-2'/>
+                <div className='py-24 px-3.5 mt-6 bg-gray-900/65 animate-pulse rounded-md min-w-full flex-1 my-2'/>
+            </div>
+        </section>
+    </>
+  )
+}
